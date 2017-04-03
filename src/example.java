32c6101cbbcb7e4f93ebd13c2eb00438c989a119
jrviray <jrviray@cpp.edu>
2017-04-03 12:30:20 -0700

    Changed oout to out

--- src/example.java
+++ src/example.java
@@ -5,7 +5,7 @@ public class example {
 
     public static void main(String[] args){
         System.out.print("Hello World!");
-	System.oout.println("This is a test");
+	System.out.println("This is a test");
     }
 
     //comment to show commits and sytuff
